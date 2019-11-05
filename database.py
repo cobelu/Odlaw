2aa9d4ee69aa416ea45f613235c730ae9fa9446c
cobelu <imamac96@gmail.com>
2019-11-05 11:17:40 -0500

    Report gen work

--- database.py
+++ database.py
@@ -53,9 +53,13 @@ class Database:
 
         :param user_id: The desired user
         :param user_table: The table of users
-        :return: JSON text of the user report
+        :return: A dictionary of pandas DFs for all user report sub-tables
         """
-        return
+        # Start from user table and search to all dependent data
+        # Key of the dict is the name of the table
+        # Value of the dict is the user's table contents
+        # report_df = report_helper(user_id, user_table, user_table)
+        return {}
 
     def report_helper(self, key_id, from_table, to_table, from_col, to_col):
         """
@@ -66,13 +70,43 @@ class Database:
         :param to_table: The table at the head of the arc
         :param from_col: The column including the key_id in from_table
         :param to_col: The column including the key_id in to_table
-        :return: JSON object of user data
+        :return: pandas DF of data
         """
+        # Form search
         report_q = "SELECT * FROM %s WHERE %s=%s" % (to_table, from_col, to_col)
         report = self.connector.query(report_q)
+
         # new_id = PK(results)
         #   BFS from new-from-table to each new-to-table:
         #   new-from-col = edge.from-col
         #   new-to-col = edge.to-col
         #   search-for-user-data(new-from-table, new-to-table, id, new-from-col, new-to-col)
         return
+
+    def generate_csv_user_data_report(self, user_id, user_table, location=None, prefix='report', sep=','):
+        """
+        Calls generate_user_data_report and writes the resulting tables to a CSV file.
+
+        :param prefix: Prefix of the report name
+        :param location: The folder to output the saved contents
+        :param user_id: The desired user's unique ID
+        :param user_table: The table of users
+        :param sep: The delimiter to be used in the output CSV file
+        :return: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html
+        """
+        saves = []
+        tables = self.generate_user_data_report(user_id, user_table)
+        for table in tables:
+            # Fetch name of table
+            table_name = 'TABLE_NAME'
+            output_url = "%s/%s_%s.csv" % (location, prefix, table_name)
+            did_save = table.to_csv(output_url, sep=sep)
+            saves.append(did_save)
+        # TODO: Zip up data-frames to a single zip archive?
+        # https://stackoverflow.com/questions/25064347/sending-multiple-csv-files-to-zip-without-storing-to-disk-in-python
+        # We were successful if all saves went through
+        if all(save is not None for save in saves):
+            return True
+        # We were unsuccessful if there was a failure
+        else:
+            return False
